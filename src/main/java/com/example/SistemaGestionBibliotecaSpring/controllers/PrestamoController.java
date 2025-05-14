fe9407599578167f2a9fb0ba4e046bf4656c2668
Tadedp <tadeodp22@gmail.com>
2025-05-14 12:45:54 -0300

    Documentación finalizada.

--- src/main/java/com/example/SistemaGestionBibliotecaSpring/controllers/PrestamoController.java
+++ src/main/java/com/example/SistemaGestionBibliotecaSpring/controllers/PrestamoController.java
@@ -19,11 +19,26 @@ public class PrestamoController {
         this.prestamoService = prestamoService;
     }
 
+    /*
+    GET /api/prestamos
+    ---
+    Obtener todos los prestamos
+    ---
+    200 OK - Devuelve lista de prestamos
+     */
     @GetMapping
     public ResponseEntity<List<Prestamo>> obtenerTodos() {
         return ResponseEntity.status(200).body(prestamoService.obtenerTodos());
     }
 
+    /*
+    GET /api/prestamos/{id}
+    ---
+    Buscar préstamo por id
+    ---
+    200 OK - Devuelve el préstamo correspondiente al id
+    404 Not Found - Sin contenido
+     */
     @GetMapping("/{id}")
     public ResponseEntity<Prestamo> buscarPorId(@PathVariable Long id) {
         try {
@@ -33,6 +48,16 @@ public class PrestamoController {
         }
     }
 
+    /*
+    POST /api/prestamos/por-libro
+    ---
+    Buscar préstamo por libro
+    ---
+    Recibe libro buscado en el cuerpo de la solicitud
+    ---
+    200 OK - Devuelve el préstamo correspondiente al libro
+    404 Not Found - Sin contenido
+     */
     @PostMapping("/por-libro")
     public ResponseEntity<Prestamo> buscarPorLibro(@RequestBody Libro libro) {
         try {
@@ -42,6 +67,16 @@ public class PrestamoController {
         }
     }
 
+    /*
+    POST /api/prestamos/por-usuario
+    ---
+    Buscar préstamo por usuario
+    ---
+    Recibe usuario buscado en el cuerpo de la solicitud
+    ---
+    200 OK - Devuelve el préstamo correspondiente al usuario
+    404 Not Found - Sin contenido
+     */
     @PostMapping("/por-usuario")
     public ResponseEntity<Prestamo> buscarPorUsuario(@RequestBody Usuario usuario) {
         try {
@@ -51,11 +86,30 @@ public class PrestamoController {
         }
     }
 
+    /*
+    POST /api/prestamos
+    ---
+    Crear préstamo
+    ---
+    Recibe préstamo a crear en el cuerpo de la solicitud
+    ---
+    200 OK - Devuelve el préstamo creado
+     */
     @PostMapping
     public ResponseEntity<Prestamo> crear(@RequestBody Prestamo prestamo) {
         return ResponseEntity.status(200).body(prestamoService.guardar(prestamo));
     }
 
+    /*
+    PUT /api/prestamos/{id}
+    ---
+    Modificar préstamo por id
+    ---
+    Recibe préstamo modificado en el cuerpo de la solicitud
+    ---
+    200 OK - Devuelve el préstamo modificado
+    404 Not Found - Sin contenido
+     */
     @PutMapping("/{id}")
     public ResponseEntity<Prestamo> actualizar(@PathVariable Long id, @RequestBody Prestamo prestamo) {
         try {
@@ -65,6 +119,14 @@ public class PrestamoController {
         }
     }
 
+    /*
+    DELETE /api/prestamos/{id}
+    ---
+    Eliminar préstamo por id
+    ---
+    204 No Content - Sin contenido
+    404 Not Found - Sin contenido
+     */
     @DeleteMapping("/{id}")
     public ResponseEntity<Void> eliminar(@PathVariable Long id) {
         try {
