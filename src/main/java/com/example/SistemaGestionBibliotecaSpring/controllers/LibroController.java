21fcc066053481f9df1eebd6c267bac4b883f2f1
Tadedp <tadeodp22@gmail.com>
2025-05-14 10:18:34 -0300

    Controladores REST implementados.

---
+++ src/main/java/com/example/SistemaGestionBibliotecaSpring/controllers/LibroController.java
@@ -0,0 +1,19 @@
+package com.example.SistemaGestionBibliotecaSpring.controllers;
+
+import com.example.SistemaGestionBibliotecaSpring.exceptions.LibroNoEncontradoException;
+import com.example.SistemaGestionBibliotecaSpring.models.Libro;
+import com.example.SistemaGestionBibliotecaSpring.services.LibroService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/libros")
+public class LibroController {
+    private final LibroService libroService;
+
+    public LibroController(LibroService libroService) {
+        this.libroService = libroService;
+    }
+}
