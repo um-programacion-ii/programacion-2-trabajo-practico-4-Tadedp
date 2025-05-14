954ebc81756410dbe080deae5d53450868168cde
Tadedp <tadeodp22@gmail.com>
2025-05-14 10:23:21 -0300

    Endpoints de controlador implementados.

--- src/main/java/com/example/SistemaGestionBibliotecaSpring/controllers/UsuarioController.java
+++ src/main/java/com/example/SistemaGestionBibliotecaSpring/controllers/UsuarioController.java
@@ -16,4 +16,51 @@ public class UsuarioController {
     public UsuarioController(UsuarioService usuarioService) {
         this.usuarioService = usuarioService;
     }
+
+    @GetMapping
+    public ResponseEntity<List<Usuario>> obtenerTodos() {
+        return ResponseEntity.status(200).body(usuarioService.obtenerTodos());
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Usuario> buscarPorId(@PathVariable Long id) {
+        try {
+            return ResponseEntity.status(200).body(usuarioService.buscarPorId(id));
+        } catch (UsuarioNoEncontradoException e) {
+            return ResponseEntity.status(404).body(null);
+        }
+    }
+
+    @GetMapping("/por-email/{email}")
+    public ResponseEntity<Usuario> buscarPorEmail(@PathVariable String email) {
+        try {
+            return ResponseEntity.status(200).body(usuarioService.buscarPorEmail(email));
+        } catch (UsuarioNoEncontradoException e) {
+            return ResponseEntity.status(404).body(null);
+        }
+    }
+
+    @PostMapping
+    public ResponseEntity<Usuario> crear(@RequestBody Usuario usuario) {
+        return ResponseEntity.status(200).body(usuarioService.guardar(usuario));
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Usuario> actualizar(@PathVariable Long id, @RequestBody Usuario usuario) {
+        try {
+            return ResponseEntity.status(200).body(usuarioService.actualizar(id, usuario));
+        } catch (UsuarioNoEncontradoException e) {
+            return ResponseEntity.status(404).body(null);
+        }
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> eliminar(@PathVariable Long id) {
+        try {
+            usuarioService.eliminar(id);
+            return ResponseEntity.status(204).body(null);
+        } catch (UsuarioNoEncontradoException e) {
+            return ResponseEntity.status(404).body(null);
+        }
+    }
 }
